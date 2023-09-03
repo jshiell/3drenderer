bb6eac63b2dabd8b010ba55e0edb4c6a8b3abc36
Jamie Shiell <jamie@infernus.org>
2023-09-03 17:03:15 +0100

    Simplified face reading, as we can assume optional face params are always present for this course

--- src/mesh.c
+++ src/mesh.c
@@ -11,15 +11,6 @@ mesh_t mesh = {
     .rotation = { .x = 0, .y = 0, .z = 0 }
 };
 
-int read_face(FILE* fp, char* buffer) {
-    fscanf(fp, "%s", buffer);
-    char* slash_char = strchr(buffer, '/');
-    if (slash_char != NULL) {
-        *slash_char = '\0';
-    }
-    return atoi(buffer);
-}
-
 bool load_obj_file_data(char* filename) {
     FILE* fp = fopen(filename, "r");
     if (fp == NULL) {
@@ -35,11 +26,8 @@ bool load_obj_file_data(char* filename) {
             fscanf(fp, "%f %f %f", &vertex.x, &vertex.y, &vertex.z);
             array_push(mesh.vertices, vertex);
         } else if (strcmp(buffer, "f") == 0) {
-            face_t face = {
-                .a = read_face(fp, buffer),
-                .b = read_face(fp, buffer),
-                .c = read_face(fp, buffer)
-            };
+            face_t face;
+            fscanf(fp, "%d/%*d/%*d %d/%*d/%*d %d/%*d/%*d", &face.a, &face.b, &face.c);
             array_push(mesh.faces, face);
         }
     }
