a6e6b768f2027b4dd6752947595ba670aa1af07a
Jamie Shiell <jamie@infernus.org>
2023-10-01 15:06:31 +0100

    Lesson: Coding the Triangle Midpoint Computation

--- src/triangle.c
+++ src/triangle.c
@@ -1,1 +1,37 @@
 #include "triangle.h"
+
+void int_swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t colour) {
+
+}
+
+void fill_flat_top_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t colour) {
+
+}
+
+void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t colour) {
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+    }
+    if (y1 > y2) {
+        int_swap(&y1, &y2);
+        int_swap(&x1, &x2);
+    }
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+    }
+
+    // find mid-point using triangle similarity
+    int my = y1;
+    int mx = (float) ((x2 - x0) * (y1 - y0)) / (float) (y2 - y0) + x0;
+
+    fill_flat_bottom_triangle(x0, y0, x1, y1, mx, my, colour);
+    fill_flat_top_triangle(x1, y1, mx, my, x2, y2, colour);
+}
