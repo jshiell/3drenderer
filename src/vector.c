972386cf560ce4043678f57cde69d6455c70b1b3
Jamie Shiell <jamie@infernus.org>
2023-10-01 13:18:34 +0100

    Exercise: Finding the Normal Vector

--- src/vector.c
+++ src/vector.c
@@ -76,6 +76,15 @@ vec3_t vec3_div(vec3_t v, float factor) {
     return results;
 }
 
+vec3_t vec3_cross(vec3_t a, vec3_t b) {
+    vec3_t result = {
+        .x = a.y * b.z - a.z * b.y,
+        .y = a.z * b.x - a.x * b.z,
+        .z = a.x * b.y - a.y * b.x
+    };
+    return result;
+}
+
 vec3_t vec3_rotate_x(vec3_t v, float angle) {
     vec3_t rotated_vector = {
         .x = v.x,
