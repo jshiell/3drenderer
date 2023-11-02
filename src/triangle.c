29118eaf0674d29697eab40869939cf29cb7fab1
Jamie Shiell <jamie@infernus.org>
2023-11-02 13:44:58 +0000

    Exercise: Textured Flat-Bottom Triangle

--- src/triangle.c
+++ src/triangle.c
@@ -1,11 +1,6 @@
 #include "triangle.h"
 #include "display.h"
-
-void int_swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+#include "swap.h"
 
 void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t colour) {
     draw_line(x0, y0, x1, y1, colour);
@@ -77,7 +72,46 @@ void draw_textured_triangle(
     int x2, int y2, float u2, float v2,
     uint32_t* texture
 ) {
-    // TODO
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+        float_swap(&u0, &u1);
+        float_swap(&v0, &v1);
+    }
+    if (y1 > y2) {
+        int_swap(&y1, &y2);
+        int_swap(&x1, &x2);
+        float_swap(&u1, &u2);
+        float_swap(&v1, &v2);
+    }
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+        float_swap(&u0, &u1);
+        float_swap(&v0, &v1);
+    }
+
+    // render flat-bottom triangle
+    float inv_slope_1 = 0;
+    float inv_slope_2 = 0;
+
+    if (y1 - y0 != 0) inv_slope_1 = (float) (x1 - x0) / abs(y1 - y0);
+    if (y2 - y0 != 0) inv_slope_2 = (float) (x2 - x0) / abs(y2 - y0);
+
+    if (y1 - y0 != 0) {
+        for (int y = y0; y <= y1; ++y) {
+            int x_start = x1 + (y - y1) * inv_slope_1;
+            int x_end = x0 + (y - y0) * inv_slope_2;
+
+            if (x_end < x_start) {
+                int_swap(&x_start, &x_end);
+            }
+
+            for (int x = x_start; x < x_end; ++x) {
+                draw_pixel(x, y, 0xFFFF00FF);
+            }
+        }
+    }
 }
 
 int triangle_compare_avg_depth(const void* a, const void* b) {
